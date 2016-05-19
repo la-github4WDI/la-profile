8924631a081a9b22072a10ba7403463929b5b13a
lee allan <lee.allan71@gmail.com>
2016-05-19 16:00:46 +0800

    1st commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// init Isotope
+var $container = $('.portfolio-items').isotope('layout');
+
+// filter items on button click
+$('.portfolio-filter').on( 'click', 'a', function(e) {
+  e.preventDefault()
+  var filterValue = $(this).attr('data-filter');
+  $container.isotope({ filter: filterValue });
+
+  $('.portfolio-filter li').removeClass('active');
+  $(this).closest('li').addClass('active');
+
+});
